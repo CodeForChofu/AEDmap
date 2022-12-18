fb39abfb3f7d2cf18bf5e6c647666ac21714a6a1
Junya Ishihara <junya@champierre.com>
2022-12-18 23:46:27 +0900

    更新日によってマーカーを色分けした。

--- map/generate-geojson.js
+++ map/generate-geojson.js
@@ -1,6 +1,7 @@
 import { OSMQuery } from "@toriyama/osmql";
 import Autolinker from 'autolinker';
 import fs from 'fs';
+import moment from 'moment';
 
 const osmQuery = new OSMQuery();
 const query = osmQuery.fromQLFile("./query.ql");
@@ -16,6 +17,7 @@ function convert(geojson) {
 			type: "Feature",
       geometry: feature.geometry,
       properties: {
+				'marker-color': getMarkerColor(feature),
 				description: description(feature),
       },
 		};
@@ -35,6 +37,21 @@ function description(feature) {
 		const value = Autolinker.link(tags[key], {stripPrefix: false});
 		desc += `<strong>${key}:</strong> ${value}<br />`;
 	}
+	desc += `<strong>updated at:</strong> ${moment(feature.properties.meta.timestamp).fromNow()}<br />`;
 	desc += `<p><a href="https://www.openstreetmap.org/edit#map=19/${lat}/${lng}" target="_blank">編集(OSM)</a></p>`
 	return desc;
+}
+
+function getMarkerColor(feature) {
+	const updateAt = feature.properties.meta.timestamp;
+	const now = moment();
+	const diff = now.diff(updateAt, 'years');
+
+	if (diff < 1) {
+		return "green";
+	} else if (diff < 3) {
+		return "orange";
+	} else {
+		return "red";
+	}
 }
